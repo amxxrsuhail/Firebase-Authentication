6e50f8a124c0c11c34752f15496c80ca1c274992
Ameer Suhail <116158760+amxxrsuhail@users.noreply.github.com>
2023-02-05 15:42:25 +0530

    import auth guard and auth redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+//Guards
+import { AuthGuard } from './shared/guard/auth.guard';
+import { AuthRedirect } from './shared/guard/authredirect.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-in', component: SignInComponent, canActivate:[AuthRedirect] },
   { path: 'register-user', component: SignUpComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
 @NgModule({
